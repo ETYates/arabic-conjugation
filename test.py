cae1e33beaa1666db5e99f50904f5b6347ec61d2
ETYates <ethan.t.yates@gmail.com>
2022-03-06 09:51:20 -0500

    Removed annotation generator from test.py

--- test.py
+++ test.py
@@ -2,22 +2,6 @@ import unittest
 
 from arabic_verb import Radical, Conjugation, FeatureNames
 
-"""
-def test():
-    names = FeatureNames()
-    conj = Conjugation('qtlua')
-    for pattern in range(conj.k+1):
-        for mood in range(conj.j+1):
-            for voice in range(conj.l+1):
-                for person in conj.i:
-                    print(names.int2pattern[pattern], end=' ')
-                    print(names.int2voice[voice], end=' ')
-                    print(names.int2mood[mood], end=' ')
-                    print(person, end=' ')
-                    print(person, mood, pattern, voice)
-                    print(conj.conjugate(person, mood, pattern, voice))
-test()
-"""
 class TestPresentTense(unittest.TestCase):
 
     def test_third_person_masc_singular(self):
